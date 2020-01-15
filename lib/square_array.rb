999e55af9964bc2c6fe0b1c62319266c9a3705f2
Jack T <jackmt9@gmail.com>
2020-01-15 03:23:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   squared = []
   counter = 0
-  while array[counter]
-    squared[counter] = array[counter] ** 2
-  end
-  return squared
+  # while array[counter]
+  #   squared[counter] = array[counter] ** 2
+  # end
+  # return squared
 end
